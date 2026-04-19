7fcf502171b260dd53501daae9abc73b9e4200b9
Maithreyan D <maithreyan@Maithreyans-MacBook-Air.local>
2026-04-19 09:55:57 +0530

    chore: commit all current changes

--- lib/widgets/app_logo.dart
+++ lib/widgets/app_logo.dart
@@ -6,41 +6,20 @@ class AppLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 90,
-      height: 90,
+      width: 220,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(26),
-        gradient: const LinearGradient(
-          colors: [Color(0xFF6C63FF), Color(0xFF00C9FF), Color(0xFFFF6FD8)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
         boxShadow: [
           BoxShadow(
-            color: const Color(0xFF6C63FF).withValues(alpha: 0.4),
-            blurRadius: 24,
-            offset: const Offset(0, 12),
+            color: const Color(0xFF5A72FF).withValues(alpha: 0.35),
+            blurRadius: 30,
+            spreadRadius: 2,
+            offset: const Offset(0, 14),
           ),
         ],
       ),
-      child: const Stack(
-        alignment: Alignment.center,
-        children: [
-          Icon(
-            Icons.camera_alt_rounded,
-            color: Colors.white,
-            size: 38,
-          ),
-          Positioned(
-            right: 18,
-            top: 18,
-            child: Icon(
-              Icons.auto_awesome_rounded,
-              color: Colors.white,
-              size: 16,
-            ),
-          ),
-        ],
+      child: const Image(
+        image: AssetImage('assets/images/image.png'),
+        fit: BoxFit.contain,
       ),
     );
   }
